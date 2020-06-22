a891dbe366a1cb8c8d7369268d40fc802b09968f
Jamie Broussard <jbroussard@Jamies-MacBook-Pro.local>
2020-06-22 08:21:55 -0600

    utility sed updated

--- vars/utility.groovy
+++ vars/utility.groovy
@@ -6,6 +6,6 @@ def sedYaml(yamlfile, env_yaml, deployment_image, environment){
   // copy yaml from Git so we can sed on it
   sh "cp ${yamlfile} ${env_yaml}"
   // replace image name with sed on new yaml for this env
-  sh "sed -i 's/{{ORIGINAL_IMAGE}}/${deployment_image}/g' ${env_yaml}"
+  sh "sed -i 's/{{IMAGE}}/${deployment_image}/g' ${env_yaml}"
   sh "sed -i 's/{{ENVIRONMENT}}/${environment}/g' ${env_yaml}"
 }
