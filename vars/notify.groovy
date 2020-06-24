617f91280bbd95ca973de5ca279d542f48a09942
Jamie Broussard <jamie@jamiebroussard.com>
2020-06-24 08:35:55 -0600

    refactor mailtext

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,10 +1,11 @@
 #!/usr/local/bin/env groovy
 def emailext(email_to){
-  emailext
+  emailext(
     body: """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
       <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""",
     subject: "STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
     to: "${email_to}"
+    )
 }
 
 // TODO ADD recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
